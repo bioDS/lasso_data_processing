3e082ecb63b276748737a85b16784b5172e8ab96
Kieran Elmes <kelmes@cs.otago.ac.nz>
2022-10-03 15:44:06 +1300

    wip: roc plots (still need to include true negatives)

--- bench_pint_whinter.sh
+++ bench_pint_whinter.sh
@@ -18,7 +18,7 @@ run_sets_on_methods() {
       if ! [ -d $set_dir ]; then
         mkdir -p $set_dir
       fi
-      taskset -c $threads bash -c "./process_rds_whinter.R $bench_sets_dir/$bench_set $set_dir $methods $num_features" &> $set_dir/log.txt
+      taskset -c $threads bash -c "./process_rds_whinter.R $bench_sets_dir/$bench_set $set_dir $methods $num_features $depth" &> $set_dir/log.txt
       #echo taskset -c $threads bash -c "./process_rds_whinter.R $bench_sets_dir/$bench_set $set_dir $methods $num_features"
     done
   done
@@ -28,28 +28,37 @@ run_sets_on_methods() {
 #set="simulated_small_data"
 
 # to reproduce current paper figures:
-num_features=1000
-sets="simulated_small_data_sample"
-methods="all"
-threads="0"
-
-run_sets_on_methods
+#depth=2
+#num_features=1000
+#sets="simulated_small_data_sample"
+#methods="all"
+#threads="0"
 #
+#run_sets_on_methods
+##
+#num_features=1000
+#sets="8k_only"
+#methods="all"
+#threads="0-7,16-23"
+#run_sets_on_methods
+#
+## new wide comparison
+#
+#num_features=1000
+#sets="wide_only"
+#methods="noglint"
+#
+#threads="0-7,16-23"
+#run_sets_on_methods
+
+# three-way comparison
 num_features=1000
-sets="8k_only"
+sets="3way"
 methods="all"
-threads="0-7,16-23"
-run_sets_on_methods
-
-# new wide comparison
-
-num_features=1000
-sets="wide_only"
-methods="noglint"
+depth="3"
 
 threads="0-7,16-23"
 run_sets_on_methods
-
 
 # testing extra things
 # num_features=60
--- plot_comparison_roc.R
+++ plot_comparison_roc.R
@@ -10,9 +10,63 @@ source("summary_functions.R")
 
 registerDoMC(cores=detectCores())
 
-bench_sets <- c("simulated_small_data_sample", "8k_only", "wide_only")
+bench_sets <- c("simulated_small_data_sample", "8k_only", "wide_only", "3way")
+
+plot_rocs <- function(all_pint_smrys, all_glint_smrys, all_whinter_smrys, use_glint, use_type, include_FN, output_filename) {
+    ## main only
+    if (use_type != "all") {
+        pint <- all_pint_smrys |> filter(type == use_type)
+        if (use_glint) {
+            glint <- all_glint_smrys |> filter(type == use_type)
+        }
+        whinter <- all_whinter_smrys |> filter(type == use_type)
+    } else {
+        pint <- all_pint_smrys
+        if (use_glint) {
+            glint <- all_glint_smrys
+        }
+        whinter <- all_whinter_smrys
+    }
+
+    if (include_FN == FALSE) {
+      pint <- pint |> filter(found == TRUE)
+      glint <- glint |> filter(found == TRUE)
+      whinter <- whinter |> filter(found == TRUE)
+    }
+
+    pint_roc <- roc(response = pint$TP, predict = abs(pint$strength))
+    whinter_roc <- roc(response = whinter$TP, predict = abs(whinter$strength))
+    if (use_glint) {
+        glint_roc <- roc(response = glint$TP, predict = abs(glint$strength))
+    }
+
+    if (use_glint) {
+      roc_list = list(Pint = pint_roc,
+                   Whinter = whinter_roc,
+                   Glinternet = glint_roc)
+      glint_annotation =  sprintf("Glinternet auc: %.2f", round(glint_roc$auc, digits = 2))
+    } else {
+      roc_list = list(Pint = pint_roc,
+                   Whinter = whinter_roc)
+      glint_annotation = ""
+    }
+    roc_plot <-
+        ggroc(roc_list) +
+        theme_bw() +
+        geom_segment(x = -1, y = 0, xend = 0, yend = 1, color = "#4c72b0") +
+        annotate("text", x = 0.2, y = 0.3, label = sprintf("Pint auc: %.2f", round(pint_roc$auc, digits = 2))) +
+        annotate("text", x = 0.2, y = 0.2, label = sprintf("Whinter auc: %.2f", round(whinter_roc$auc, digits = 2))) +
+        annotate("text", x = 0.2, y = 0.1, label = glint_annotation)
+    ggsave(roc_plot, file = output_filename, width = 5, height = 3)
+}
 
 for (bench_set in bench_sets) {
+    if (bench_set == "wide_only") {
+        use_glint = FALSE
+    } else {
+        use_glint = TRUE
+    }
+
     dir <- paste("whinter_pint_comparison", bench_set, sep = "/")
 
     rds_files <- list.files(dir, pattern = "all.rds", recursive = TRUE, full.names = TRUE)
@@ -28,52 +82,11 @@ for (bench_set in bench_sets) {
     all_whinter_smrys <- foreach(set = output, .combine = rbind) %do% { set$whinter_smry }
     all_glint_smrys <- foreach(set = output, .combine = rbind) %do% { set$glint_smry }
 
-    # Including False Negatives
-    pint_roc <- roc(response = all_pint_smrys$TP, predict = abs(all_pint_smrys$strength))
-    whinter_roc <- roc(response = all_whinter_smrys$TP, predict = abs(all_whinter_smrys$strength))
-    glint_roc <- roc(response = all_glint_smrys$TP, predict = abs(all_glint_smrys$strength))
+    plot_rocs(all_pint_smrys, all_glint_smrys, all_whinter_smrys, use_glint, "all", TRUE, sprintf("plots/rocs/all_comparison_roc_%s.pdf", bench_set))
+    plot_rocs(all_pint_smrys, all_glint_smrys, all_whinter_smrys, use_glint, "main", TRUE, sprintf("plots/rocs/main_comparison_roc_%s.pdf", bench_set))
+    plot_rocs(all_pint_smrys, all_glint_smrys, all_whinter_smrys, use_glint, "interaction", TRUE, sprintf("plots/rocs/int_comparison_roc_%s.pdf", bench_set))
 
-
-    roc_plot <-
-        ggroc(list(Pint = pint_roc,
-                   Whinter = whinter_roc,
-                   Glinternet = glint_roc)) +
-        theme_bw() +
-        geom_segment(x = -1, y = 0, xend = 0, yend = 1, color = "#4c72b0") #+
-        ## annotate("text", x = 0.2, y = 0.1, label = sprintf("auc: %.2f", round(roc$auc, digits = 2)))
-    ggsave(roc_plot, file = "plots/overall_comparison_roc.pdf", width = 5, height = 3)
-
-    ## main only
-    pint_main <- all_pint_smrys |> filter(type == "main")
-    glint_main <- all_glint_smrys |> filter(type == "main")
-    whinter_main <- all_whinter_smrys |> filter(type == "main")
-    pint_main_roc <- roc(response = pint_main$TP, predict = abs(pint_main$strength))
-    whinter_main_roc <- roc(response = whinter_main$TP, predict = abs(whinter_main$strength))
-    glint_main_roc <- roc(response = glint_main$TP, predict = abs(glint_main$strength))
-
-    roc_plot <-
-        ggroc(list(Pint = pint_roc,
-                   Whinter = whinter_roc,
-                   Glinternet = glint_roc)) +
-        theme_bw() +
-        geom_segment(x = -1, y = 0, xend = 0, yend = 1, color = "#4c72b0") #+
-        ## annotate("text", x = 0.2, y = 0.1, label = sprintf("auc: %.2f", round(roc$auc, digits = 2)))
-    ggsave(roc_plot, file = "plots/main_comparison_roc.pdf", width = 5, height = 3)
-
-    ## interaction only
-    pint_interaction <- all_pint_smrys |> filter(type == "interaction")
-    glint_interaction <- all_glint_smrys |> filter(type == "interaction")
-    whinter_interaction <- all_whinter_smrys |> filter(type == "interaction")
-    pint_interaction_roc <- roc(response = pint_interaction$TP, predict = abs(pint_interaction$strength))
-    whinter_interaction_roc <- roc(response = whinter_interaction$TP, predict = abs(whinter_interaction$strength))
-    glint_interaction_roc <- roc(response = glint_interaction$TP, predict = abs(glint_interaction$strength))
-
-    roc_plot <-
-        ggroc(list(Pint = pint_interaction_roc,
-                   Whinter = whinter_interaction_roc,
-                   Glinternet = glint_interaction_roc)) +
-        theme_bw() +
-        geom_segment(x = -1, y = 0, xend = 0, yend = 1, color = "#4c72b0") #+
-        ## annotate("text", x = 0.2, y = 0.1, label = sprintf("auc: %.2f", round(roc$auc, digits = 2)))
-    ggsave(roc_plot, file = "plots/interaction_comparison_roc.pdf", width = 5, height = 3)
+    plot_rocs(all_pint_smrys, all_glint_smrys, all_whinter_smrys, use_glint, "all", FALSE, sprintf("plots/rocs/all_comparison_roc_%s_FoundOnly.pdf", bench_set))
+    plot_rocs(all_pint_smrys, all_glint_smrys, all_whinter_smrys, use_glint, "main", FALSE, sprintf("plots/rocs/main_comparison_roc_%s_FoundOnly.pdf", bench_set))
+    plot_rocs(all_pint_smrys, all_glint_smrys, all_whinter_smrys, use_glint, "interaction", FALSE, sprintf("plots/rocs/int_comparison_roc_%s_FoundOnly.pdf", bench_set))
 }
