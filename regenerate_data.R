b73c8014eb6285735c9eb5869a666a7d538c4601
Kieran Elmes <kieran.elmes@postgrad.otago.ac.nz>
2022-10-05 02:05:49 +0000

    include 3way data in regenerate script

--- regenerate_data.R
+++ regenerate_data.R
@@ -10,7 +10,7 @@ registerDoMC(cores=detectCores())
 
 source("generation_functions.R")
 
-generate_sets <- function(path, num, size = "large") {
+generate_sets <- function(path, num, size = "large", threeway = FALSE) {
     viol = 100
     num_lethals = 0
     if (size == "8k") {
@@ -34,9 +34,30 @@ generate_sets <- function(path, num, size = "large") {
         num_bi = 100
         num_bij = 500
         num_bijk = 0
+    } else if (size == "large_3way") {
+        n = 40000
+        p = 4000
+        snr = 5
+        num_bi = 10
+        num_bij = 100
+        num_bijk = 1000
+    } else if (size == "small_3way") {
+        n = 4000
+        p = 400
+        snr = 5
+        num_bi = 10
+        num_bij = 100
+        num_bijk = 1000
+    } else if (size == "wide_3way") {
+        n = 1000
+        p = 10000
+        snr = 5
+        num_bi = 10
+        num_bij = 100
+        num_bijk = 1000
     }
     else {
-        print("size must be either large, wide, or small")
+        print("invalid size")
     }
     for (e in 1:num) {
         dataset = generate_set(n, p, snr, num_bi, num_bij, num_bijk, num_lethals)
@@ -75,6 +96,17 @@ ensure_wide_set_exists <- function(path) {
     }
 }
 
+ensure_3way_set_exists <- function(path) {
+    if(!dir.exists(path)) {
+        dir.create(path, recursive = TRUE)
+    }
+    existing_files = list.files(path)
+    if (length(existing_files) == 0) {
+        generate_sets(path, 10, "small_3way", threeway = TRUE)
+    }
+}
+
 ensure_p100_set_exists("./data/simulated_rerun/simulated_small_data_sample/")
 ensure_8k_set_exists("./data/simulated_rerun/8k_only/")
 ensure_wide_set_exists("./data/simulated_rerun/wide_only/")
+ensure_3way_set_exists("./data/simulated_rerun/3way")
