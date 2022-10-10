a07324c94407c393c67ce358865c939b67a779c4
Kieran Elmes <kieran.elmes@postgrad.otago.ac.nz>
2022-10-10 21:20:05 +0000

    specify depth in bench script

--- bench_pint_whinter.sh
+++ bench_pint_whinter.sh
@@ -18,8 +18,7 @@ run_sets_on_methods() {
       if ! [ -d $set_dir ]; then
         mkdir -p $set_dir
       fi
-      taskset -c $threads bash -c "./process_rds_whinter.R $bench_sets_dir/$bench_set $set_dir $methods $num_features $depth" &> $set_dir/log.txt
-      #echo taskset -c $threads bash -c "./process_rds_whinter.R $bench_sets_dir/$bench_set $set_dir $methods $num_features"
+      taskset -c $taskset_threads bash -c "./process_rds_whinter.R $bench_sets_dir/$bench_set $set_dir $methods $num_features $depth $use_cores" &> $set_dir/log.txt
     done
   done
 }
@@ -28,37 +27,43 @@ run_sets_on_methods() {
 #set="simulated_small_data"
 
 # to reproduce current paper figures:
+
 depth=2
 num_features=1000
 sets="simulated_small_data_sample"
 methods="all"
-threads="0"
-
+taskset_threads="0"
+use_cores="1"
 run_sets_on_methods
+
 ##
-num_features=1000
-sets="8k_only"
-methods="all"
-threads="0-95"
-run_sets_on_methods
-
-# new wide comparison
-
-num_features=1000
-sets="wide_only"
-methods="noglint"
-
-threads="0-95"
-run_sets_on_methods
+#depth=2
+#num_features=5000
+#sets="8k_only"
+#methods="all"
+#taskset_threads="0-95"
+#use_cores="96"
+#run_sets_on_methods
+#
+## new wide comparison
+#
+#num_features=10000
+#sets="wide_only"
+#methods="noglint"
+#
+#taskset_threads="0-95"
+#use_cores="1"
+#run_sets_on_methods
 
 # three-way comparison
-num_features=1000
-sets="3way"
-methods="all"
-depth="3"
-
-threads="0-95"
-run_sets_on_methods
+#num_features=79800
+#sets="3way"
+#methods="all"
+#depth="3"
+#
+#taskset_threads="0-95"
+#use_cores="1"
+#run_sets_on_methods
 
 # testing extra things
 # num_features=60
